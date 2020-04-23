0c669101c715f7c372e783b01b37144a132b79ce
HaAnhHung <hahunguetk62@gmail.com>
2020-04-23 09:56:41 +0700

    test

---
+++ app/src/main/java/com/example/barberbooking/Interface/IBookingInfoLoadListener.java
@@ -0,0 +1,9 @@
+package com.example.barberbooking.Interface;
+
+import com.example.barberbooking.Model.BookingInformation;
+
+public interface IBookingInfoLoadListener {
+    void onBookingInfoLoadEmpty();
+    void onBookingInfoLoadSuccess(BookingInformation bookingInformation);
+    void onBookingInfoLoadFailed(String message);
+}
